5a7797b789cd59e9f9b819468f21f945241f282d
Bruno DEGUIL <bruno.deguil@gmail.com>
2021-04-18 19:49:41 +0200

    Initial Commit

---
+++ metroboulot/metroboulotApp.swift
@@ -0,0 +1,17 @@
+//
+//  metroboulotApp.swift
+//  metroboulot
+//
+//  Created by Bruno Deguil on 18/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct metroboulotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
